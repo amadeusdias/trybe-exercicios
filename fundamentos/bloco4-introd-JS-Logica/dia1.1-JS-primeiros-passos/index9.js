a0839e953baf82432767de45c619e4be55f1c58f
amadeus <amadeusdias@hotmail.com>
2022-03-08 14:36:16 -0300

    exercicio do dojo

---
+++ fundamentos/bloco4-introd-JS-Logica/dia1.1-JS-primeiros-passos/index9.js
@@ -0,0 +1,16 @@
+let number = 4;
+
+
+    switch(number){
+    case 1:
+        console.log("Trybe Lanche Feliz");
+        break;
+    case 2:
+        console.log("McTrybe");
+        break;
+    case 3:
+        console.log("TrybeWooper");
+        break;
+    case 4:
+        console.log("X-Trybe");    
+}    
