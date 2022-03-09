339223e75654ad21269ce37695d7dc03c952f76a
amadeus <amadeusdias@hotmail.com>
2022-03-09 19:25:08 -0300

    exercicios do 7 ao 9, corrige o exercicio 5

---
+++ fundamentos/bloco4-introd-JS-Logica/dia2-array-loop-for/script8.js
@@ -0,0 +1,12 @@
+let numeros = [];
+for(let i = 1; i <= 25; i++){
+    numeros.push(i)
+}
+// console.log(numeros);
+
+let porDois =[];
+for(let i =0; i <= numeros.length; i++) {
+    porDois.push(numeros[i] / 2)
+}
+
+console.log(porDois);
