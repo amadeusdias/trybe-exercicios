c845526dbce3c568770d25b00135a1429d5396df
amadeus <amadeusdias@hotmail.com>
2022-04-22 20:04:16 -0300

    Parte 1 dos exercicios do dia 2

---
+++ fundamentos/bloco7-introdução-a-JS-ES6-teste-unitarios/dia2-ES6-fluxos-de-exceção-e-objetos/exemplos.js
@@ -0,0 +1,52 @@
+const coolestTvShow = {
+    name: "BoJack Horseman",
+    genre: "adult animation",
+    createdBy: "Raphael Bob-Waksberg",
+    favoriteCharacter: "Princess Carolyn",
+    quote: "Princess Carolyn always lands on her feet.",
+    seasons: 6,
+  };
+  console.log(Object.keys(coolestTvShow));
+  console.log(Object.values(coolestTvShow));
+  console.log(Object.entries(coolestTvShow));
+
+  const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
+  
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', // chave adicionada
+  };
+  const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for (const index in arrayOfSkills) {
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    }
+  };
+  
+  console.log('Estudante 1');
+  listSkills(student1);
+  
+  console.log('Estudante 2');
+  listSkills(student2);
+
+
+  const band = {
+    name: 'Blind Guardian',
+    formedIn: 1986,
+    lastAlbum: 'Beyond the Red Mirror',
+  };
+  
+  const info = {
+    genre: 'Power Metal',
+    lastAlbum: 'Twilight Orchestra: Legacy of the Dark Lands',
+  };
+
+  console.log(Object.assign(band, info));
