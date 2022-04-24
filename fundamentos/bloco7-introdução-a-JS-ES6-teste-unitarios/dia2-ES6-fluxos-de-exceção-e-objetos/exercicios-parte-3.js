0a20fc44de241c8365003bf10e0133785addf9e9
amadeus <amadeusdias@hotmail.com>
2022-04-24 16:47:50 -0300

    finalização dos exercicios parte3

--- fundamentos/bloco7-introdução-a-JS-ES6-teste-unitarios/dia2-ES6-fluxos-de-exceção-e-objetos/exercicios-parte-3.js
+++ fundamentos/bloco7-introdução-a-JS-ES6-teste-unitarios/dia2-ES6-fluxos-de-exceção-e-objetos/exercicios-parte-3.js
@@ -27,4 +27,40 @@ console.log(Object.entries(lesson2))
 const list = (obj) => Object.keys(obj);
 
 console.log(list(lesson3));
-  
+
+const listTamanho = (obj) => Object.keys(obj).length;
+
+console.log(listTamanho(lesson3));
+
+const listValues = (obj) => Object.values(obj);
+console.log(listValues(lesson1));
+
+const allLessons = Object.assign({}, {lesson1,lesson2, lesson3});
+console.log(allLessons);
+
+function getStudents(lessons) {
+  const keys = Object.keys(lessons);
+  let total = 0;
+  for(let i = 0; i < keys.length; i += 1) {
+    const currentKey = keys[i];
+    total += lessons[currentKey].numeroEstudantes
+    }
+    return total;
+}
+console.log(getStudents(allLessons));
+
+const getValueByNumber = (obj, index) => Object.values(obj)[index]
+console.log(getValueByNumber(lesson2, 2));
+//a função Object.values retorna um array com os valores do objeto escolhido, ao colocar o [] apos a escrita da função, você seleciona qual dos valores do objeto quer mostrar.
+// a função Object.keys também retorna um array com as chaves do objeto. E pode ser acessado da mesma forma. 
+
+function verifyPar(obj, key, valor) {
+  const arrei = Object.entries(obj);
+  let result = false;
+  for(let i = 0; i < arrei.length; i += 1){
+  if(arrei[i][0] === key && arrei[i][1] === valor)
+  result = true;
+}
+return result;
+}
+console.log(verifyPar(lesson1, 'turno', 'manhã'));
