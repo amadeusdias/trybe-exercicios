9c9d6dcbe4f44416d3653454770fc3d0fb378676
Amadeus Dias <amadeus@pop-os.localdomain>
2022-05-19 21:56:26 -0300

    exercicos dia 8.5 completos e comentados

--- fundamentos/bloco8-HOFs-JS-ES6/dia5-spread-operator-param-rest-e-destructuring/exercicio6.js
+++ fundamentos/bloco8-HOFs-JS-ES6/dia5-spread-operator-param-rest-e-destructuring/exercicio6.js
@@ -6,5 +6,22 @@ const palio = ['Palio', 'Fiat', 2019];
 const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
-// escreva toObject abaixo
+// escreva toObject abaiixo.
 
+const toObject = ( [ first, second, third ] ) => {
+    return {
+        name: first,
+        marca: second,
+        ano: third
+    }
+}
+
+console.log(toObject(chiron))
+
+// refatoração de acordo com o gabarito: 
+
+const toObject2 = ( [nome, brand, year] ) => ({ nome, brand, year});
+
+console.log(toObject2(shelbyCobra));
+
+// na refatoração a vantagem é usar a abreviação ao setar os parametros com o mesmo nome das chaves que voce quer dar ao objeto, dessa forma o JS automaticamente lê os mesmos nomes e constroi o objeto com chaves e valores iguais. A utilização de array destructuring faz com que os valores do objeto construido fiquem dinamicos, mudando de acordo com o parametro passado.
